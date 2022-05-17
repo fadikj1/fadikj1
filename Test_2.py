3e45417ffbb18612b9be62197967b0fc559b49ca
fadikj1 <fadiken@hotmail.com>
2022-05-17 23:40:34 +0200

    my first work

---
+++ Test_2.py
@@ -0,0 +1,6 @@
+a = 9
+b = 7
+
+
+print(a != b)
+print(type(a == b))
---
+++ test 2.py
@@ -0,0 +1,13 @@
+example = [2, 3, 4, 9, 99]
+
+example.append(96)
+example.append(22)
+example.append(82)
+
+
+print(example)
+print(example[0])
+print(example[2])
+print(example[4])
+print(example[5])
+
